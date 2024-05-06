1d3b0d2ecb6bb3601ca92dee25388d7606701d05
SillyJoMoney <galloj1@roverkids.org>
2024-05-06 14:18:29 -0400

    Challendge faze 1

--- PlayFetchC/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ PlayFetchC/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -9,7 +9,6 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
-
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
 
@@ -25,8 +24,9 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
     }
 
 }
